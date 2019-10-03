68870d091ca1f41f5e6e6b1007acf62c086362b7
crypcoder <crypcoder@gmail.com>
2019-10-03 19:47:14 -0400

    Corrected short/long window range issue.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import itertools
 import time
-from simulations.monte_carlo import ParallelMonteCarloSimulation
+from simulations.monte_carlo import ParallelMonteCarloSimulation, MonteCarloSimulation
 
 
 def chunk(it, size):
@@ -22,31 +22,31 @@ def chunk(it, size):
 
 if __name__ == "__main__":
 
-    # read in price data
-    data = pd.read_csv('F.csv', index_col='Date', parse_dates=True)
+    """read in price data"""
+    data = pd.read_csv('data\\F.csv', index_col='Date', parse_dates=True)
 
-    # generate our list of possible short window length inputs
+    """generate our list of possible short window length inputs"""
     short_mas = np.linspace(20, 50, 30, dtype=int)
 
-    # generate our list of possible long window length inputs
+    """generate our list of possible long window length inputs"""
     long_mas = np.linspace(100, 200, 30, dtype=int)
 
-    # generate a list of tuples containing all combinations of
-    # long and short window length possibilities
+    """generate a list of tuples containing all combinations of
+        long and short window length possibilities"""
     mas_combined = list(itertools.product(short_mas, long_mas))
 
-    # use our helper function to split the moving average tuples list
-    # into slices of length 180
+    """use helper function to split the moving average tuples list
+        into slices of length 180"""
     mas_combined_split = list(chunk(mas_combined, 180))
 
-    # set requried number of MC simulations per backtest optimisation
-    iters = 2000
+    """set requried number of MC simulations per backtest optimisation"""
+    numberOfIterations = 2000
 
-    # start timer
+    """start timer"""
     start_time = time.time()
 
-    # call our multi-threaded function
-    results = ParallelMonteCarloSimulation(data, mas_combined_split, iters).run()
-
-    # print number of seconds the process took
+    """call our multi-threaded function"""
+    results = ParallelMonteCarloSimulation(data, mas_combined_split, numberOfIterations).run()
+    #results = MonteCarloSimulation(data, mas_combined_split, numberOfIterations).run(mas_combined_split)
+    """print number of seconds the process took"""
     print("MP--- %s seconds for para---" % (time.time() - start_time))
--- simulations/monte_carlo.py
+++ simulations/monte_carlo.py
@@ -1,9 +1,10 @@
 import numpy as np
-from multiprocessing.pool import ThreadPool as Pool
+from multiprocessing.pool import ThreadPool, Pro as Pool
+
 
 class MonteCarloSimulation(object):
 
-    def __init__(self, data, inputs, iters, days=252):
+    def __init__(self, data, inputs, numberOfIterations, days=252, spreadDiffThreshold=5):
         """ Base MonteCarlo Simulation
 
         Parameters:
@@ -14,18 +15,22 @@ class MonteCarloSimulation(object):
        """
         self.data = data
         self.inputs = inputs
-        self.iters = iters
+        self.numberOfIterations = numberOfIterations
         self.days = days
+        self.spreadDiffThreshold = spreadDiffThreshold
 
-
-    def annualisedSharpe(self, returns, N=252):
-        """To calculate Sharpe Ratio - Risk free rate element excluded for simplicity
+    def annualisedSharpe(self, returns, numberOfHistoricalDays=252):
+        """To calculate Sharpe Ratio - Sharpe ratio measures the performance of an
+            investment compared to a risk-free asset, after adjusting for its risk.
+            Risk free rate element excluded for simplicity
 
         Parameters:
         returns (DataFrame): Strategy data
         N (int): Number of historical days for simulation
        """
-        return np.sqrt(N) * (returns.mean() / returns.std())
+        if returns.std() == 0:
+            return 0
+        return np.sqrt(numberOfHistoricalDays) * (returns.mean() / returns.std())
 
     def movingAverageStrategy(self, shortMovingAvg, longMovingAvg):
         """To calculate Moving Average Strategy
@@ -35,32 +40,27 @@ class MonteCarloSimulation(object):
         longMovingAvg (int):
        """
         data = self.data
-        #create columns with MA values
-        data['short_ma'] = np.round(data['Close'].rolling(window=shortMovingAvg).mean(),2)
-        data['long_ma'] = np.round(data['Close'].rolling(window=longMovingAvg).mean(),2)
+        """create columns with MA values"""
+        data['short_ma'] = np.round(data['Close'].rolling(window=shortMovingAvg).mean(), 2)
+        data['long_ma'] = np.round(data['Close'].rolling(window=longMovingAvg).mean(), 2)
 
-        #create column with moving average spread differential
+        """create column with moving average spread differential"""
         data['short_ma-long_ma'] = data['short_ma'] - data['long_ma']
 
-        #set desired number of points as threshold for spread difference and create column containing strategy 'Stance'
-        X = 5
-        data['Stance'] = np.where(data['short_ma-long_ma'] > X, 1, 0)
-        data['Stance'] = np.where(data['short_ma-long_ma'] < -X, -1, data['Stance'])
+        """create column containing strategy 'Stance'"""
+        data['Stance'] = np.where(data['short_ma-long_ma'] > self.spreadDiffThreshold, 1, 0)
+        data['Stance'] = np.where(data['short_ma-long_ma'] < -self.spreadDiffThreshold, -1, data['Stance'])
         data['Stance'].value_counts()
 
-        #create columns containing daily market log returns and strategy daily log returns
+        """create columns containing daily market log returns and strategy daily log returns"""
         data['Market Returns'] = np.log(data['Close'] / data['Close'].shift(1))
         data['Strategy'] = data['Market Returns'] * data['Stance'].shift(1)
 
-        #set strategy starting equity to 1 (i.e. 100%) and generate equity curve
+        """set strategy starting equity to 1 (i.e. 100%) and generate equity curve"""
         data['Strategy Equity'] = data['Strategy'].cumsum()
 
-        #calculate Sharpe Ratio
-        #try/except to escape case of division by zero
-        try:
-            sharpe = self.annualisedSharpe(data['Strategy'])
-        except:
-            sharpe = 0
+        """calculate Sharpe Ratio"""
+        sharpe = self.annualisedSharpe(data['Strategy'])
 
         return data['Strategy'].cumsum(), sharpe, data['Strategy'].mean(), data['Strategy'].std()
 
@@ -68,10 +68,11 @@ class MonteCarloSimulation(object):
         """To start the simulation
 
         :param inputSlices:
-        :return: Moving Average
+        :return: input data, performance, sharpe ratio, mu, sigma, mc_results, mc_results_final_val
         """
         # iterate through the slice of the overall MA window tuples list that
         # has been passed to this thread
+
         for input_slice in inputSlices:
             # use the current inputs to backtest the strategy and record
             # various results metrics
@@ -81,7 +82,7 @@ class MonteCarloSimulation(object):
             mc_results = []
             mc_results_final_val = []
             # run the specified number of MC simulations and store relevant results
-            for j in range(self.iters):
+            for j in range(self.numberOfIterations):
                 daily_returns = np.random.normal(mu, sigma, self.days) + 1
                 price_list = [1]
                 for x in daily_returns:
@@ -91,19 +92,20 @@ class MonteCarloSimulation(object):
                 mc_results.append(price_list)
                 # store only the ending value of each individual price path
                 mc_results_final_val.append(price_list[-1])
-        return (inputSlices, perf, sharpe, mu, sigma, mc_results, mc_results_final_val)
+        return inputSlices, perf, sharpe, mu, sigma, mc_results, mc_results_final_val
+
 
 class ParallelMonteCarloSimulation(MonteCarloSimulation):
 
-    def __init__(self, data, inputs, iters, days=252):
+    def __init__(self, data, inputs, numberOfIterations, days=252):
         """Multi-threading implementation of Monte Carlo simulation
 
         :param data:
         :param inputs:
-        :param iters:
+        :param numberOfIterations:
         :param days:
         """
-        super(ParallelMonteCarloSimulation, self).__init__(data, inputs, iters, days=days)
+        super(ParallelMonteCarloSimulation, self).__init__(data, inputs, numberOfIterations, days=days)
 
     def run(self):
         """Start the Parallel Monte Carlo simulations
